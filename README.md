b66a1bb5bb58d4253399a8dc58c7f147b473cf82
Thomas Guyader <thomas.guyader@gouv.nc>
2024-01-04 12:38:59 +1100

    chore: import initial du module

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # gcp-dataflow-to-bigquery
 Module terraform pour transférer des tables on premise vers BigQuery
+
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+No requirements.
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_google"></a> [google](#provider\_google) | n/a |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [google_cloud_scheduler_job.jo](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_scheduler_job) | resource |
+| [google_project_iam_custom_role.dataflow-custom-role](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/project_iam_custom_role) | resource |
+| [google_project_iam_member.bigquery_editor_bindings](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/project_iam_member) | resource |
+| [google_project_iam_member.bigquery_user_bindings](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/project_iam_member) | resource |
+| [google_project_iam_member.dataflow_custom_worker_bindings](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/project_iam_member) | resource |
+| [google_project_iam_member.dataflow_worker_bindings](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/project_iam_member) | resource |
+| [google_project_service.cloudschedulerapi](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/project_service) | resource |
+| [google_project_service.dataflowapi](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/project_service) | resource |
+| [google_project_service.secretmanagerapi](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/project_service) | resource |
+| [google_service_account.service_account](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/service_account) | resource |
+| [google_service_account_iam_member.gce-default-account-iam](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/service_account_iam_member) | resource |
+| [google_storage_bucket.bucket](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket) | resource |
+| [google_secret_manager_secret_version.jdbc-url-secret](https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/secret_manager_secret_version) | data source |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_dataset_name"></a> [dataset\_name](#input\_dataset\_name) | nom du projet | `string` | n/a | yes |
+| <a name="input_group_name"></a> [group\_name](#input\_group\_name) | Google groupe associé au projet | `string` | n/a | yes |
+| <a name="input_isTruncate"></a> [isTruncate](#input\_isTruncate) | Si vrai, vide la table cible, si faux, ajoute les données à l'existant | `bool` | `true` | no |
+| <a name="input_jdbc-url-secret-name"></a> [jdbc-url-secret-name](#input\_jdbc-url-secret-name) | nom du secret contenant l'url de connexion jdbc à la BDD | `string` | n/a | yes |
+| <a name="input_notification_channels"></a> [notification\_channels](#input\_notification\_channels) | canal de notification pour les alertes sur dataproc | `list(string)` | n/a | yes |
+| <a name="input_project_id"></a> [project\_id](#input\_project\_id) | id du projet | `string` | n/a | yes |
+| <a name="input_queries"></a> [queries](#input\_queries) | n/a | `map(map(string))` | n/a | yes |
+| <a name="input_region"></a> [region](#input\_region) | n/a | `string` | `"europe-west1"` | no |
+| <a name="input_schedule"></a> [schedule](#input\_schedule) | expression cron de schedule du job | `string` | n/a | yes |
+| <a name="input_type_database"></a> [type\_database](#input\_type\_database) | type de base de données: oracle ou postgresql | `string` | n/a | yes |
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
---
+++ main.tf
@@ -0,0 +1,130 @@
+locals {
+  secret-managment-project = "prj-dinum-p-secret-mgnt-aaf4"
+}
+
+resource "google_service_account" "service_account" {
+  account_id   = "sa-${var.dataset_name}"
+  display_name = "Service Account created by terraform for ${var.project_id}"
+  project      = var.project_id
+}
+
+resource "google_project_iam_member" "bigquery_editor_bindings" {
+  project = var.project_id
+  role    = "roles/bigquery.dataEditor"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_project_iam_member" "bigquery_user_bindings" {
+  project = var.project_id
+  role    = "roles/bigquery.user"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_project_iam_member" "dataflow_worker_bindings" {
+  project = var.project_id
+  role    = "roles/dataflow.worker"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_service_account_iam_member" "gce-default-account-iam" {
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.service_account.email}"
+  service_account_id = google_service_account.service_account.name
+}
+
+resource "google_project_iam_custom_role" "dataflow-custom-role" {
+  project     = var.project_id
+  role_id     = "dataflow_custom_role"
+  title       = "Dataflow Custom Role"
+  description = "Role custom pour pouvoir créer des job dataflow depuis scheduler"
+  permissions = ["iam.serviceAccounts.actAs", "dataflow.jobs.create"]
+}
+
+resource "google_project_iam_member" "dataflow_custom_worker_bindings" {
+  project    = var.project_id
+  role       = "projects/${var.project_id}/roles/${google_project_iam_custom_role.dataflow-custom-role.role_id}"
+  member     = "serviceAccount:${google_service_account.service_account.email}"
+  depends_on = [google_project_iam_custom_role.dataflow-custom-role]
+}
+
+####
+# Bucket
+####
+
+resource "google_storage_bucket" "bucket" {
+  project                     = var.project_id
+  name                        = "bucket-${var.dataset_name}"
+  location                    = var.region
+  storage_class               = "REGIONAL"
+  uniform_bucket_level_access = true
+}
+
+####
+# Dataflow
+####
+resource "google_project_service" "dataflowapi" {
+  project = var.project_id
+  service = "dataflow.googleapis.com"
+}
+
+resource "google_project_service" "secretmanagerapi" {
+  project = var.project_id
+  service = "secretmanager.googleapis.com"
+}
+
+resource "google_project_service" "cloudschedulerapi" {
+  project = var.project_id
+  service = "cloudscheduler.googleapis.com"
+}
+
+data "google_secret_manager_secret_version" "jdbc-url-secret" {
+  project = local.secret-managment-project
+  secret  = var.jdbc-url-secret-name
+}
+
+resource "google_cloud_scheduler_job" "jo" {
+  for_each         = var.queries
+  project          = var.project_id
+  name             = "job-${var.dataset_name}-${each.key}"
+  schedule         = "${index(keys(var.queries), each.key) % 60} ${var.schedule}"
+  time_zone        = "Pacific/Noumea"
+  attempt_deadline = "320s"
+
+  retry_config {
+    retry_count = 1
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://dataflow.googleapis.com/v1b3/projects/${var.project_id}/locations/${var.region}/flexTemplates:launch"
+    oauth_token {
+      service_account_email = "${google_service_account.service_account.email}"
+    }
+    body = base64encode(
+      jsonencode(
+        {
+          launchParameter : {
+            jobName : "df-${var.dataset_name}-${each.key}",
+            containerSpecGcsPath : "gs://dataflow-templates-${var.region}/latest/flex/Jdbc_to_BigQuery_Flex",
+            parameters : {
+              driverJars : "gs://bucket-${var.dataset_name}/ojdbc8-21.7.0.0.jar,gs://bucket-${var.dataset_name}/postgresql-42.2.6.jar",
+              driverClassName : "${var.type_database == "oracle" ? "oracle.jdbc.driver.OracleDriver" : "org.postgresql.Driver"}",
+              connectionURL : data.google_secret_manager_secret_version.jdbc-url-secret.secret_data,
+              query : each.value.query,
+              outputTable : "${var.project_id}:${var.dataset_name}.${each.value.bigquery_location}",
+              bigQueryLoadingTemporaryDirectory : "gs://bucket-${var.dataset_name}/tmp",
+              isTruncate : var.isTruncate,
+              stagingLocation : "gs://dataflow-staging-${var.region}-419271540634/staging",
+              serviceAccount : google_service_account.service_account.email,
+            },
+            environment : {
+              numWorkers : 2,
+              tempLocation : "gs://dataflow-staging-${var.region}-419271540634/tmp",
+              subnetwork : "regions/${var.region}/subnetworks/subnet-for-vpn",
+            }
+          }
+        }
+      )
+    )
+  }
+}
